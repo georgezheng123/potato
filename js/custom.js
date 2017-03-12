e499e54d58fea887d0a2c9515192fcf21893629a
jkao1 <jkao1@stuy.edu>
2017-03-11 22:40:40 -0500

    fix scrolling on editorpicks, make stuff prettier, json

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,69 @@
-var departments = [ 'arts and entertainment', 'art', 'business', 'copy', 'features', 'humor', 'layout', 'news', 'opinions', 'photo', 'sports', 'web' ];
+var departments = [ 'ae', 'art', 'business', 'copy', 'features', 'humor', 'layout', 'news', 'opinions', 'photo', 'sports', 'web' ];
 
 /* horizontal bar */
-var horizontalBar = document.getElementsByClassName("horizontal-bar-row")[0];
-for (var d = departments.length-1; d >= 0; d--) {
-    var td = horizontalBar.insertCell(0);
-    var elementId = "scrollTo-" + departments[d];
-    td.setAttribute('id',elementId);
-    td.setAttribute('class','scrollMe');
-    if (departments[d] === 'arts and entertainment') {
-        td.innerHTML = '<a href="">Arts&amp;Entertainment';
-    } else {
-        td.innerHTML = '<a href="">' + departments[d].charAt(0).toUpperCase() + departments[d].slice(1);
+function showHorizontalBar() {
+    var horizontalBar = document.getElementsByClassName("horizontal-bar-row")[0];
+    for (var d = departments.length-1; d >= 0; d--) {
+        var td = horizontalBar.insertCell(0);
+        var scrollToElement = "scrollTo-" + departments[d];
+        td.setAttribute('data-scrollTo',scrollToElement);
+        td.setAttribute('class','scrollMe');
+        if (departments[d] === 'ae') {
+            td.innerHTML = 'Arts&amp;Entertainment';
+        } else {
+            td.innerHTML = departments[d].charAt(0).toUpperCase() + departments[d].slice(1);
+        }
     }
 }
 
 /* department previews */
-var departmentPreviews = document.getElementsByClassName("department-previews")[0];
-for (var d = departments.length-1; d >= 0; d--) {
-    deptName = '<h3>' + "Name" + '</h3>' //some json stuff
-    deptQuote = '<p>Quote</p>'; // some json stuff
-    deptTestimonial = '<p>Testimonial</p>'; // some json stuff
-    deptPhoto = '<img src="' + "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1200px-Glazed-Donut.jpg" + '">';
-    var table = document.createElement('table');
-    var row = table.insertRow(0);
-    if (d % 2 == 0) {
-        var larger = row.insertCell(0);
-        larger.setAttribute('class','larger');
-        larger.innerHTML = deptName + deptQuote + deptTestimonial;
-        var smaller = row.insertCell(1);
-        smaller.setAttribute('class','smaller');
-        smaller.innerHTML = deptPhoto;
-    } else {
-        var smaller = row.insertCell(0);
-        smaller.setAttribute('class','smaller');
-        smaller.innerHTML = deptPhoto;
-        var larger = row.insertCell(1);
-        larger.setAttribute('class','larger');
-        larger.innerHTML = deptName + deptQuote + deptTestimonial;
+function showDepartmentPreviews() {
+    var departmentPreviews = document.getElementsByClassName("department-previews")[0];
+    for (var d = 0; d < departments.length; d++) {
+        deptName = '<h3>' + departments[d] + '</h3>' //some json stuff
+        deptQuote = '<p>Quote</p>'; // some json stuff
+        deptTestimonial = '<p>Testimonial</p>'; // some json stuff
+        deptPhoto = '<img src="' + "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1200px-Glazed-Donut.jpg" + '">';
+        var table = document.createElement('table');
+        table.setAttribute('id',"scrollTo-" + departments[d]);
+        var row = table.insertRow(0);
+        if (d % 2 == 0) {
+            var larger = row.insertCell(0);
+            larger.setAttribute('class','larger');
+            larger.innerHTML = deptName + deptQuote + deptTestimonial;
+            var smaller = row.insertCell(1);
+            smaller.setAttribute('class','smaller');
+            smaller.innerHTML = deptPhoto;
+        } else {
+            var smaller = row.insertCell(0);
+            smaller.setAttribute('class','smaller');
+            smaller.innerHTML = deptPhoto;
+            var larger = row.insertCell(1);
+            larger.setAttribute('class','larger');
+            larger.innerHTML = deptName + deptQuote + deptTestimonial;
+        }
+        departmentPreviews.appendChild(table);
     }
-    departmentPreviews.appendChild(table);
+}
+
+/* department editor picks */
+function showEditorPicks() {
+    var editorPicks = document.getElementsByClassName("editor-picks-list")[0];
+    for (var d = 0; d < departments.length; d++) {
+        var div = document.createElement('div');
+        var deptName = document.createElement('h3');
+        deptName.innerHTML = departments[d]; //some json stuff
+        div.appendChild(deptName);
+        var deptDescription;
+        if (true) { // json stuff to see if article is img or naw
+            deptDescription = '<a href="">Article Name/img/art</a>';
+        }
+        var table = document.createElement('table');
+        table.setAttribute('id',"scrollTo-" + departments[d]);
+        table.setAttribute('class','angelaTable');
+        var row = table.insertRow(0);
+        var td = row.insertCell(0);
+        td.innerHTML = deptName + deptDescription;
+        editorPicks.appendChild(table);
+    }
 }
