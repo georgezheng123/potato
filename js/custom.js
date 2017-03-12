09c63caa0cda39d647f61a50cbd27478025b17bf
Max Z <max.zlotskiy@gmail.com>
2017-03-11 21:27:37 -0500

    put everything in an onLoad

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,18 @@
 var departments = [ 'ae', 'art', 'business', 'copy', 'features',' humor', 'layout', 'news', 'opinions', 'photo', 'sports', 'web' ];
 
+(function() {
+    var row = document.getElementsByClassName("horizontal-bar-row")[0];
 
-var row = document.getElementsByClassName("horizontal-bar-row")[0];
-
-for (var d = 0; d < departments.length; d++) {
-    var td = row.insertCell(0);
-    var elementId = "scrollTo-" + departments[d];
-    td.setAttribute('id',elementId);
-    td.setAttribute('class','scrollMe');
-    if (departments[d] === 'ae') {
-        td.innerHTML = '<a href="">A(CHANGETOAMPERSANDCODE)E';
-    } else {
-        td.innerHTML = '<a href="">' + departments[d].charAt(0).toUpperCase() + departments[d].slice(1);
+    for (var d = 0; d < departments.length; d++) {
+        var td = row.insertCell(0);
+        var elementId = "scrollTo-" + departments[d];
+        td.setAttribute('id',elementId);
+        td.setAttribute('class','scrollMe');
+        if (departments[d] === 'ae') {
+            td.innerHTML = '<a href="">A&amp;E';
+        } else {
+            td.innerHTML = '<a href="">' + departments[d].charAt(0).toUpperCase() + departments[d].slice(1) + "</a>";
+        }
     }
-}
+})();
+
